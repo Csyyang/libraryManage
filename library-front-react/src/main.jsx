fd7fe42bfb2130754e2d5fb70322e53726ed9cd0
csy <693765678@qq.com>
2024-01-12 16:08:23 +0800

    添加react router

--- library-front-react/src/main.jsx
+++ library-front-react/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import RouterApp from './router/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterApp />
   </React.StrictMode>,
 )
