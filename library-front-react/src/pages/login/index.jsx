5a2e1f722a35bcf833c045e188784985edf2e93e
csy <693765678@qq.com>
2024-01-12 17:38:53 +0800

    登录页暂存

---
+++ library-front-react/src/pages/login/index.jsx
@@ -0,0 +1,17 @@
+import { Card } from 'antd'
+import styles from './index.module.css'
+
+const Login = () => {
+    return (
+        <div className={styles.center}>
+
+            <Card style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+            </Card>
+        </div>
+    )
+}
+
+export default Login
--- library-front-react/src/router/index.jsx
+++ library-front-react/src/router/index.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from '../App'
+import Login from "../pages/login";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: App()
+        element: <App />
     },
+    {
+        path: '/login',
+        element: <Login />
+    }
 ]);
 
 const RouterApp = () => {
